e0566316e10d39dd01f91ceaa586be50cfebdfe5
shivamskr151 <shivamskr151@gmail.com>
2026-01-01 14:54:59 +0530

    Update MobileLayout.vue and clean up frontend build assets

--- frappe-bench/apps/lms/lms/public/frontend/assets/InsertVideo-BxJ2OLzc.js
+++
@@ -1,2 +0,0 @@
-import{d as s,bI as t,bJ as a,bK as d}from"./index-C4szjxrb.js";const p=s({__name:"InsertVideo",props:{editor:{}},setup(e){const o=e;function n(){o.editor.chain().focus().selectAndUploadVideo().run()}return(r,i)=>t(r.$slots,"default",a(d({onClick:n})))}});export{p as default};
-//# sourceMappingURL=InsertVideo-BxJ2OLzc.js.map
